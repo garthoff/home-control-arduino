dec7b17d35c5f93e4bb16f093d7096f0b4258f9a
Nicolas Goy <kuon@goyman.com>
2012-08-07 15:50:06 +0200

    Using system wide directories

--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@ require 'daemons'
 
 dir = File.dirname(File.expand_path(__FILE__))
 Dir.chdir(dir)
-Daemons.run('arcontrol.rb', :log_output => true)
+Daemons.run('arcontrol.rb', :log_output => true, :dir_mode => :system)
